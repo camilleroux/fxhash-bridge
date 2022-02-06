0da88dbaa319ccb016a5d349dd0b8c30966a5836
Camille Roux <25977+camilleroux@users.noreply.github.com>
2022-02-06 13:32:36 +0100

    remove commented code

--- src/styles/camilleroux.js
+++ src/styles/camilleroux.js
@@ -32,11 +32,6 @@ export default class CamilleRouxStyle extends Style {
   }
 
   drawTile (tilePoints, frontLeftCorner3DCoord, isBorder) {
-    // this._p5.noStroke() // this._p5.stroke(this.defaultColor)
-    // this._p5.fill(isBorder ? this.colors[2] : this.defaultColor)
-    // this._p5.fill(this.backgroundColor)
-    // this._p5.quad(tilePoints[0].x * this._s, tilePoints[0].y * this._s, tilePoints[1].x * this._s, tilePoints[1].y * this._s, tilePoints[2].x * this._s, tilePoints[2].y * this._s, tilePoints[3].x * this._s, tilePoints[3].y * this._s)
-
     this._p5.fill(isBorder ? this.defaultColor : this._p5.random(this.colors))
     for (let tilePointId = 0; tilePointId < tilePoints.length; tilePointId++) {
       const dotDensity = Math.pow((this._gridSizeY - frontLeftCorner3DCoord.y) / this._gridSizeY, 9)
@@ -44,11 +39,9 @@ export default class CamilleRouxStyle extends Style {
         this._p5.noStroke()
         const pointOriginVect = tilePoints[tilePointId].copy()
         const pointVect = pointOriginVect.lerp(tilePoints[(tilePointId + 1) % tilePoints.length], this._p5.random())
-        this._p5.rect(pointVect.x * this._s, (pointVect.y - Math.abs(this._p5.randomGaussian(0, 0.07))) * this._s, this._s * 0.0005)
+        this._p5.rect(pointVect.x * this._s, (pointVect.y - Math.abs(this._p5.randomGaussian(0, isBorder ? 0.1 : 0.07))) * this._s, this._s * 0.0005)
       }
     }
-    // this._p5.stroke('red')
-    // this._p5.quad(tilePoints[0].x * this._s, tilePoints[0].y * this._s, tilePoints[1].x * this._s, tilePoints[1].y * this._s, tilePoints[2].x * this._s, tilePoints[2].y * this._s, tilePoints[3].x * this._s, tilePoints[3].y * this._s)
   }
 
   static author () { return 'Camille Roux' }
