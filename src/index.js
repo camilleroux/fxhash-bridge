c423c4b00fb4d17b09efdb7812af286742613d89
Camille Roux <25977+camilleroux@users.noreply.github.com>
2022-02-08 15:33:56 +0100

    bug fix

--- src/index.js
+++ src/index.js
@@ -35,8 +35,8 @@ const borders = getWeightedOption([
 const perspective = Math.floor(FXRandomBetween(0.01, 0.08) * 100) / 100
 const missingTiles = Math.floor(FXRandomBetween(0.3, 0.8) * 10) / 10
 
-let styleClassId = FXRandomIntBetween(0, stylesClasses.length)
 const stylesClasses = [CamilleRoux2Style, CamilleRouxStyle, BoilerplateStyle, DemoStyle]
+let styleClassId = FXRandomIntBetween(0, stylesClasses.length)
 let currentStyle
 
 // defining features
