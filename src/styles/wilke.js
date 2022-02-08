15f9d8d17a46c728ceb46380eacce40a394802e5
Claus Wilke <wilke@austin.utexas.edu>
2022-02-08 01:20:25 -0600

    color and texture tweaks

--- src/styles/wilke.js
+++ src/styles/wilke.js
@@ -8,11 +8,36 @@ export default class WilkeStyle extends Style {
   constructor (gridSizeX, gridSizeY, s, projectionCalculator3d, p5) {
     super(gridSizeX, gridSizeY, s, projectionCalculator3d, p5)
     this.centerx = this._p5.random(0.45, 0.55)
-    this.centery = this._p5.random(0.6, 0.4)
-    this.radius = this._p5.random(0.2, 0.4)
+    this.centery = this._p5.random(0.32, 0.45)
+    this.radius = this._p5.random(0.2, 0.3)
+    this.strokescale = this._s / 500
     console.log("Center x:", this.centerx)
     console.log("Center y:", this.centery)
     console.log("Radius:", this.radius)
+    console.log(this._s)
+    
+    
+    // colors
+    // light scheme
+    this.bgfill = '#FEFAF0'
+    this.bgstipple = '#E9E5DB'
+    this.circlefill = '#303030'
+    this.circlestipple = '#505050'
+    
+    this.borderdark = '#811012'
+    this.borderlight = '#AB4D4E'
+    this.floordark = '#000000'
+    this.floorlight = this.bgfill
+    
+    // dark scheme
+    this.bgfill = '#202020'
+    this.bgstipple = '#050505'
+    this.circlefill = '#FEFAF0'
+    this.circlestipple = '#E9E5DB'
+    
+    this.borderlight = '#A67475'
+    this.floorlight = this.circlestipple
+    
   }
   
   drawPointInterior (x, y) {
@@ -32,24 +57,25 @@ export default class WilkeStyle extends Style {
   }
   
   beforeDraw () {
-    this._p5.background('#FEFAF0')
+    this._p5.background(this.bgfill)
     
     // canvas background stippling
-    this._p5.stroke('#F0F0F0')
-    this._p5.strokeWeight(.5)
-    let n = 400
+    this._p5.stroke(this.bgstipple)
+    this._p5.strokeWeight(.5 * this.strokescale)
+    let n = 250
     for (let i = 0; i < n; i++) {
       for (let j = 0; j < n; j++) {
-        let x = (i + 0.5) / n + this._p5.randomGaussian(0, 0.4/n)
-        let y = (j + 0.5) / n + this._p5.randomGaussian(0, 0.4/n)
+        let x = (i + 0.5) / n
+        x = x*x*(3-2*x) + this._p5.randomGaussian(0, 0.4/n)
+        let y = (j + 0.5) / n
+        y = y*y*(3-2*y) + this._p5.randomGaussian(0, 0.4/n)
         this._p5.point(x * this._s, y * this._s)
       }
     }
 
     
     this._p5.noStroke()
-    this._p5.fill('#303030')
-    this._p5.strokeWeight(2)
+    this._p5.fill(this.circlefill)
     this._p5.circle(this.centerx * this._s, this.centery * this._s, 2 * this.radius * this._s)
 
     // halo around center circle
@@ -63,8 +89,8 @@ export default class WilkeStyle extends Style {
     }*/
     
     // interior background stippling
-    this._p5.stroke('#505050')
-    this._p5.strokeWeight(.5)
+    this._p5.stroke(this.circlestipple)
+    this._p5.strokeWeight(.5 * this.strokescale)
     n = 800 * this.radius
     for (let i = 0; i < n; i++) {
       for (let j = 0; j < n; j++) {
@@ -77,28 +103,21 @@ export default class WilkeStyle extends Style {
     }
   }
 
-  drawTile (tilePoints, frontLeftCorner3DCoord) {
-    // unproject tile points
-    // https://github.com/Infl1ght/projection-3d-2d
+  drawTile (tilePoints, frontLeftCorner3DCoord, isBorder) {
     let mean = frontLeftCorner3DCoord.copy()
     mean.add(this._p5.createVector(0.5, 0.5, 0))
     
-    /*
-    this._p5.noStroke()
-    this._p5.fill('#E5E3E0')
-    this._p5.quad(tilePoints[0].x * this._s, tilePoints[0].y * this._s, tilePoints[1].x * this._s, tilePoints[1].y * this._s, tilePoints[2].x * this._s, tilePoints[2].y * this._s, tilePoints[3].x * this._s, tilePoints[3].y * this._s)
-    */
-    
     // y distance in uv coords
     let dy = tilePoints[0].y - tilePoints[2].y
-    //console.log(dy)
-    let pcol = this._p5.color('#000000')
+    // highlight borders
+    let darkcol = isBorder ? this._p5.color(this.borderdark) : this._p5.color(this.floordark)
+    let lightcol = isBorder ? this._p5.color(this.borderlight) : this._p5.color(this.floorlight)
     let alpha = 255
     if (10*dy < .2) {
       alpha = 1275*10*dy
     }
-    pcol.setAlpha(alpha)
-    let sw = 2*Math.max(10*dy, .1)
+    darkcol.setAlpha(alpha)
+    let sw = 2*Math.max(10*dy, .1) * this.strokescale
     this._p5.strokeWeight(sw)
     let off = 0.5/this._s
     // number of points along one dimension
@@ -109,43 +128,18 @@ export default class WilkeStyle extends Style {
         let x = (i + 0.5)/n + this._p5.randomGaussian(0, 0.2/n) - .5
         let y = (j + 0.5)/n + this._p5.randomGaussian(0, 0.2/n) - .5
         let p = this._projectionCalculator3d.getProjectedPoint([x + mean.x, y + mean.y, mean.z])
-        this._p5.stroke("#FFFFFF")
+        this._p5.stroke(lightcol)
         this.drawPointHalfClip(p[0] + off, p[1] + off)
-        this._p5.stroke(pcol)
+        this._p5.stroke(darkcol)
         this.drawPointHalfClip(p[0], p[1])
       }
     }
-      
-      /*
-  
-    for (let i = 0; i < 300*(1 - 0.02*frontLeftCorner3DCoord.y); i++) {
-      let x = this._p5.randomGaussian(0, .3)
-      let y = this._p5.randomGaussian(0, .3)
-      if (x > -.5 && x < .5 && y > -.5 && y < .5) {
-        let p = this._projectionCalculator3d.getProjectedPoint([x + mean.x, y + mean.y, mean.z])
-        this._p5.stroke("#FFFFFF")
-        this.drawPointHalfClip(p[0] + off, p[1] + off)
-        this._p5.stroke(pcol)
-        this.drawPointHalfClip(p[0], p[1])
-      }
-    } */
   }
 
   afterDraw () {
-    /*
-    this._p5.stroke('#0203F0')
-    this._p5.strokeWeight(2)
-    for (let i = 0; i < 5000; i++) {
-      let x = this._p5.randomGaussian(.5, .2) * this._s 
-      let y = this._p5.randomGaussian(.5, .2) * this._s
-      this._p5.point(x, y)
-    }
-    this._p5.circle(this.x * this._s, this.y * this._s, this.radius * this.s_)
-    this._p5.circle(.2 * this._s, .2 * this._s, .4 * this.s_)
-    */
   }
 
   static author () { return 'Claus O. Wilke' }
 
-  static name () { return 'Wilke' }
+  static name () { return 'Into another world' }
 }
