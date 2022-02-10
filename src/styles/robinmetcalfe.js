f738755365709dba11a03ab63a60934e19cc0a7f
Robin <robinmetcalfe@gmail.com>
2022-02-10 12:52:42 +0000

    Adding shading & more texture

--- src/styles/robinmetcalfe.js
+++ src/styles/robinmetcalfe.js
@@ -7,6 +7,8 @@ import { FXInit, FXRandomBetween, FXRandomIntBetween, getWeightedOption } from '
 import chroma from "chroma-js";
 import p5 from 'p5'
 
+// todo - scale e.g. bubbles based on screen size
+
 // Some helper functions
 // Basic lerp
 const lerp = (a, b, t) => {
@@ -21,11 +23,14 @@ const range = (number, inMin, inMax, outMin, outMax) => {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
 
+// external constant
+const BORDER_HEIGHT = 0.04
+
 FXInit(fxrand)
 
 // Some random variables which are set across all tiles
 let curtainStringModiferRange = [FXRandomBetween(-0.005, -0.001), FXRandomBetween(0.001, 0.005)]
-let heightRange = [FXRandomBetween(0.005, 0.04), FXRandomBetween(0.04, 0.25)]
+let heightRange = [FXRandomBetween(0.005, BORDER_HEIGHT), FXRandomBetween(BORDER_HEIGHT, 0.25)]
 let heightVarianceRange = [FXRandomBetween(0.01, 0.05), FXRandomBetween(0.05, 0.25)]
 let colorRange = chroma.scale(['#666', '#f00'])
 let borderHeightRange = [FXRandomBetween(2, 4), FXRandomBetween(4, 7)]
@@ -52,10 +57,16 @@ export default class RobinMetcalfeStyle extends Style {
     // re-init fxhash so that this always starts from the same
     // random seed point on e.g. window resize
     fxrand = sfc32(...hashes)
+
+    // Some shorthand accessors
+    this.prj = this._projectionCalculator3d
+    this.v = this._p5.createVector
+
   }
 
   beforeDraw () {
     this._p5.background('#eee')
+    this.background()
   }
 
 
@@ -88,6 +99,51 @@ export default class RobinMetcalfeStyle extends Style {
   }
 
 
+
+
+  background() {
+    
+    
+    let col = pal(0).darken(2).desaturate(1.1)
+    
+    this._p5.strokeWeight(0)
+
+    const center = this.v().set(this.prj.getProjectedPoint([0, this._gridSizeY, 0.1]))
+
+    for(let i = this._s * 2; i > 0; i--) {
+      let _col = col.darken(i / this._s * 2)
+      this._p5.fill(_col.hex())
+      this._p5.stroke(_col.hex())
+      this._p5.circle(center.x * this._s, center.y * this._s, i)
+
+      //const center = 
+
+      /*const c1 = this.v().set(this.prj.getProjectedPoint([-this._gridSizeX * 10, this._gridSizeY, i * 3]))
+      
+      const second = this.v().set(this.prj.getProjectedPoint([this._gridSizeX * 10, this._gridSizeY, i]))
+      const c2 = this.v().set(this.prj.getProjectedPoint([this._gridSizeX * 10, this._gridSizeY, i * 3]))*/
+      
+      /*this._p5.stroke('#006')
+      this._p5.circle(c1.x * this._s, c1.y * this._s, 10)
+      this._p5.stroke('#666')
+      this._p5.circle(first.x * this._s, first.y * this._s, 10)
+      this._p5.stroke('#666')
+      this._p5.circle(second.x * this._s, second.y * this._s, 10)
+      this._p5.stroke('#600')
+      this._p5.circle(c2.x * this._s, c2.y * this._s, 10)*/
+
+      /*this._p5.stroke(_col.hex())
+
+      this._p5.curve(
+          c1.x * this._s, c1.y * this._s,
+          first.x * this._s, first.y * this._s, 
+          second.x * this._s, second.y * this._s, 
+          c2.x * this._s, c2.y * this._s,
+      )*/
+    }
+  }
+
+
   drawTile (t, f, isBorder) {
 
     // Shorthand vars
@@ -100,8 +156,13 @@ export default class RobinMetcalfeStyle extends Style {
     // Stick with a simple 3D projection, adjust randomly based
     // on height, apply interesting effects to the "surface" of dots...
 
-    this._p5.stroke(0, 0)
-    this._p5.fill(col.desaturate(1.5).hex())
+    this._p5.strokeWeight(1)
+
+    const rowFactor = (this._gridSizeY - f.y) / this._gridSizeY
+
+    this._p5.stroke(col.desaturate((1 - rowFactor) * 3).alpha(rowFactor).hex())
+    //this._p5.fill(col.desaturate(1.5).hex())
+    this._p5.fill(0, 0)
 
     this._p5.quad(
       t[0].x * this._s, t[0].y * this._s,
@@ -150,13 +211,115 @@ export default class RobinMetcalfeStyle extends Style {
 
     const mults = 3
 
-    p5.strokeWeight((1 - howFarForward) * 2)
+    let structureStrokeWeight = (1 - howFarForward) * 3
+
+    if(f.y < 4) {
+      structureStrokeWeight = this._s / 250
+    }
+
+    
+
+
 
     // For continuing the "curtain" to the left, so from x = 0
     let firstCurtainLength
 
+    let bubbleChance = 0
+
+    if(f.y < 8)
+      bubbleChance = FXRandomBetween(0.01, 0.05) * (1 - f.y / 8)
+
+    let curtainDifference = FXRandomBetween(0.01, 0.1)
+
+
+    const bubbleRowFactor = (8 - f.y) / 8 // goes from 1 -> 0
+    const bubbleAlpha = FXRandomBetween(0.2, 1)
+
     for(let j = 0; j <= gridRes; j++) {
       for(let i = 0; i <= gridRes; i++) {
+
+
+        // Draw the bubbles
+        if(FXRandomBetween(0, 1) < bubbleChance) {
+          // spawn a source of bubbles
+          this._p5.stroke(col.brighten().hex())
+          this._p5.strokeWeight(1)
+          this._p5.fill(0, 0)
+
+          const bubbleDarken = FXRandomBetween(0, 1) * (1 - bubbleRowFactor)
+          const bubbleDesaturate = FXRandomBetween(0, (j/gridRes) * 2) * (1 - bubbleRowFactor)
+          const bubbleSize = [FXRandomBetween(0.5, 1.5), FXRandomBetween(1.5, 3)]
+          
+          let bubbleHeight = isBorder ? BORDER_HEIGHT : 0
+
+          const bubble = v().set(prj.getProjectedPoint([
+            // make the resolution of the "curtain" effect
+            // 4 times finer than the resolution of the quad
+            f.x + (i * xRes),
+            f.y + (j * yRes),
+            bubbleHeight
+          ]))
+
+
+
+          for(let k = 0; k < 25; k++) {
+
+            bubbleHeight += Math.pow(k * 0.0002, 1.05) + FXRandomBetween(-0.0005, 0.004)
+
+            // adjust within screen-space. They'll always be going directly up
+            let point = bubble.copy()
+            point = point.sub(v(0, bubbleHeight))
+            this._p5.stroke(col
+                              .darken(bubbleDarken + (k * 0.05))
+                              .alpha(((25 - k) / 25) * bubbleRowFactor * bubbleAlpha)
+                              .desaturate(bubbleDesaturate)
+                              .hex())
+            this._p5.circle(point.x * this._s, point.y * this._s, FXRandomBetween(bubbleSize[0], bubbleSize[1]) * rowFactor)
+
+          }
+
+          
+        }
+
+
+        // Draw the top part
+        // 
+        // Todo - deduplicate this!
+        const isEdge = (j <= 2 || j >= gridRes - 2 || i <= 2 || i >= gridRes - 2)
+
+        let additionalHeightModifier = range(Math.sin(t[0].x * 4), -1, 1, 0, 0.2)
+
+        let pointHeight = isBorder ?
+              height + heightModifier + additionalHeightModifier + BORDER_HEIGHT :
+              height + heightModifier + additionalHeightModifier
+
+        let offset = Math.sin(i / surfaceSinAdjust) * Math.cos(j / surfaceCosAdjust)
+        pointHeight += range(offset, -1, 1, -surfaceAmplitude, surfaceAmplitude)              
+        
+        let pointCol = col.darken((gridRes - j) / gridRes).desaturate(Math.abs(0.75 - height))
+        if(isEdge) {
+          pointCol = pointCol.darken(0.2)
+        }
+
+        const point = v().set(prj.getProjectedPoint([
+                f.x + (i * xRes),
+                f.y + (j * yRes),
+                pointHeight
+              ]))        
+
+        this._p5.stroke(pointCol.hex())
+        this._p5.circle(point.x * this._s, point.y * this._s, 1)
+
+
+      }
+    }
+
+    for(let j = 0; j <= gridRes; j++) {
+      for(let i = 0; i <= gridRes; i++) {
+
+        
+
+        this._p5.strokeWeight(structureStrokeWeight)
 
         // determine distance from edges
         const isEdge = (j <= 2 || j >= gridRes - 2 || i <= 2 || i >= gridRes - 2)
@@ -164,7 +327,7 @@ export default class RobinMetcalfeStyle extends Style {
         let additionalHeightModifier = range(Math.sin(t[0].x * 4), -1, 1, 0, 0.2)
 
         let pointHeight = isBorder ?
-              height + heightModifier + additionalHeightModifier + 0.04 :
+              height + heightModifier + additionalHeightModifier + BORDER_HEIGHT :
               height + heightModifier + additionalHeightModifier
 
         let offset = Math.sin(i / surfaceSinAdjust) * Math.cos(j / surfaceCosAdjust)
@@ -220,31 +383,25 @@ export default class RobinMetcalfeStyle extends Style {
           //topQuad.push(point.y)
         }
 
-        let pointCol = col.darken((gridRes - j) / gridRes)
-        if(isEdge) {
-          pointCol = pointCol.darken(0.1)
-        }
 
-        p5.stroke(pointCol.hex())
-        p5.circle(point.x * this._s, point.y * this._s, 1)
 
         // Draw a "curtain" effect hanging down from the front
         // of each quad
         if(j == 0 || (t[0].x < 0.5 && i == gridRes) || (t[0].x > 0.5 && i == 0)) {
           
-          let curtainCol = col
+          let curtainCol = col.desaturate(Math.abs(0.75 - height))
 
           if(j !== 0 && (i == 0 || i == gridRes)) {
-            curtainCol = curtainCol.darken(1 + (j / gridRes)* 2)
+            curtainCol = curtainCol.darken(1 + (j / gridRes) * 2)
           }
 
           // Shine
-          const shineFactor = Math.sin(i % (j + 5)) * Math.cos(Math.pow(i, 1.2))
-          curtainCol = curtainCol.brighten(shineFactor * 3)
+          const shineFactor = Math.sin(t[0].x * 2) * Math.cos(t[0].y * 4)
+          curtainCol = curtainCol.brighten(shineFactor / 3)
 
-          p5.stroke(curtainCol
-                      .brighten(Math.sin(j % (i + 1)) * Math.cos(j * gridRes))
-                      .darken(Math.sin(j * 4 / ((i + 1) * 8)) * Math.cos(4 * i + j))
+          this._p5.stroke(curtainCol
+                      //.brighten(Math.sin(j % (i + 1)) * Math.cos(j * gridRes))
+                      //.darken(Math.sin(j * 4 / ((i + 1) * 8)) * Math.cos(4 * i + j))
                       .hex())
 
           for(let k = 0; k < 1; k++) {
@@ -295,41 +452,48 @@ export default class RobinMetcalfeStyle extends Style {
             this.lerpLine({
               from: curtainStart,
               to: curtainEnd,
-              fromColor: col.hex(),
-              toColor: col.saturate(2).hex(),
+              fromColor: curtainCol.hex(),
+              toColor: curtainCol.saturate(2).hex(),
               segments: gridRes
             })
 
-            /*curtainCol = curtainCol.desaturate(1.25).brighten(1.25)
+            
 
-            p5.stroke(curtainCol
+
+            /*if(f.y < 4) {
+
+              curtainCol = curtainCol.desaturate(1.25).brighten(1.25)
+
+              this._p5.stroke(curtainCol
                       .brighten(Math.sin(j % (i + 1)) * Math.cos(j * gridRes))
-                      .darken(Math.sin(j * 4 / ((i + 1) * 8)) * Math.cos(4 * i + j))
-                      .hex())*/
+                      .desaturate(Math.sin(j * 4 / ((i + 1) * 8)) * Math.cos(4 * i + j))
+                      .alpha(f.y / 4)
+                      .hex())
 
-            /*let floorCurtainStart = v().set(prj.getProjectedPoint([
-              // make the resolution of the "curtain" effect
-              // 4 times finer than the resolution of the quad
-              f.x + (i * xRes + (k * (xRes / 4))),
-              f.y + (j * yRes),
-              isBorder ? 0.04 : 0
-            ]))
+              let floorCurtainStart = v().set(prj.getProjectedPoint([
+                // make the resolution of the "curtain" effect
+                // 4 times finer than the resolution of the quad
+                f.x + (i * xRes + (k * (xRes / 4))),
+                f.y + (j * yRes),
+                isBorder ? BORDER_HEIGHT : 0
+              ]))
 
-            let floorCurtainEnd = v().set(prj.getProjectedPoint([
-              // make the resolution of the "curtain" effect
-              // 4 times finer than the resolution of the quad
-              f.x + (i * xRes + (k * (xRes / 4))),
-              f.y + (j * yRes),
-              (pointHeight - lastCurtainLength) - FXRandomBetween(0.01, 0.1)
-            ]))
+              let floorCurtainEnd = v().set(prj.getProjectedPoint([
+                // make the resolution of the "curtain" effect
+                // 4 times finer than the resolution of the quad
+                f.x + (i * xRes + (k * (xRes / 4))),
+                f.y + (j * yRes),
+                ((pointHeight - lastCurtainLength) - curtainDifference) / 2
+              ]))
 
-            this.lerpLine({
-              from: floorCurtainStart,
-              to: floorCurtainEnd,
-              fromColor: col.hex(),
-              toColor: col.saturate(2).hex(),
-              segments: gridRes
-            })*/
+              this.lerpLine({
+                from: floorCurtainStart,
+                to: floorCurtainEnd,
+                fromColor: curtainCol.hex(),
+                toColor: curtainCol.saturate(2).hex(),
+                segments: gridRes
+              })
+            }
 
             /*p5.line(
               floorCurtainStart.x * this._s,
@@ -345,6 +509,7 @@ export default class RobinMetcalfeStyle extends Style {
 
       }
     }
+
 
  
     return
