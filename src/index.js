7a1f6e97d8dd76527e04055480e7d666811493d2
Estienne <info@estienne.ca>
2022-02-14 17:52:13 -0500

    Added EstienneStyle

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ import RobinMetcalfeStyle from './styles/robinmetcalfe'
 import AnaglyphicStyle from './styles/anaglyphic'
 import frederativeStyle from './styles/frederative'
 import DavidEsqStyle from './styles/davidesq'
+import EstienneStyle from './styles/estienne'
 
 // note about the fxrand() function
 // when the "fxhash" is always the same, it will generate the same sequence of
@@ -46,6 +47,7 @@ const perspective = Math.floor(FXRandomBetween(0.01, 0.08) * 100) / 100
 const missingTiles = Math.floor(FXRandomBetween(0.3, 0.8) * 10) / 10
 
 const stylesClasses = [
+  EstienneStyle,
   frederativeStyle,
   DavidEsqStyle,
   ShuhblamStyle,
